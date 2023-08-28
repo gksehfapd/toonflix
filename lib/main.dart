94e40e26d39ae4167d2e7b7efb325d76706f3a14
gksehfapd <1ehfapd@gmail.com>
2023-08-29 00:48:11 +0900

    test: Icon, Transform

--- lib/main.dart
+++ lib/main.dart
@@ -120,6 +120,7 @@ class App extends StatelessWidget {
                 height: 20,
               ),
               Container(
+                  clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                       color: const Color(0xFF1F2123),
                       borderRadius: BorderRadius.circular(25)),
@@ -128,6 +129,7 @@ class App extends StatelessWidget {
                       30,
                     ),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
@@ -161,8 +163,19 @@ class App extends StatelessWidget {
                                       fontSize: 20,
                                     )),
                               ],
-                            )
+                            ),
                           ],
+                        ),
+                        Transform.scale(
+                          scale: 2.2,
+                          child: Transform.translate(
+                            offset: const Offset(-5, 12),
+                            child: const Icon(
+                              Icons.euro_rounded,
+                              color: Colors.white,
+                              size: 88,
+                            ),
+                          ),
                         )
                       ],
                     ),
