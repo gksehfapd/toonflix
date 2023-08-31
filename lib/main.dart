4075a539a3ae87673297f6610c074db71494920b
gksehfapd <1ehfapd@gmail.com>
2023-08-31 21:30:07 +0900

    test: setState

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,12 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
+  List<int> numbers = [];
 
   void onClicked() {
     setState(() {
-      counter = counter + 1;
+      numbers.add(numbers.length);
+      print(numbers);
     });
   }
 
@@ -42,10 +43,7 @@ class _AppState extends State<App> {
                 'Click Count',
                 style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+              for (var n in numbers) Text('$n'),
               IconButton(
                 iconSize: 40,
                 onPressed: onClicked,
